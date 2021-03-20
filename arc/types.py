25f49105052b72cf64a1a3b07abec4f82c794a6c
ikkamens <ikkamens@amazon.com>
2021-03-20 14:23:56 +0100

    Enable adding a title to an ARC plot

--- arc/types.py
+++ arc/types.py
@@ -30,8 +30,10 @@ class ArcIOPair:
         self.x = x
         self.y = y
 
-    def plot(self, show=True):
+    def plot(self, show=True, title:str=None):
         fig, (ax1, ax2) = plt.subplots(1, 2)
+        if title:
+            fig.suptitle(title, fontsize=16)
         plot_grid(ax1, self.x)
         ax1.set_title("Input")
         plot_grid(ax2, self.y)
