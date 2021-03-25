1dad4626350773944d868f4b808d14892c3d3765
ikkamens <ikkamens@amazon.com>
2021-03-25 17:41:34 +0100

    Added test to verify direction of axis in Numpy vs WebUI

---
+++ test/test_numpy_repr.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+from arc import get_train_problem_by_uid
+from arc import ArcColors as ac
+
+
+drop_prob = get_train_problem_by_uid("54d82841")
+
+
+def test_axes():
+    """expects to have origin in upper-left corner, x axis to go down, y axis to go right.
+
+    See the drop problem in ARC web interface for reference."""
+
+    has_drops = drop_prob.train_pairs[0].y
+
+    assert np.all(has_drops[-1] == [0, 0, ac.YELLOW, 0, 0, 0, ac.YELLOW, 0])
